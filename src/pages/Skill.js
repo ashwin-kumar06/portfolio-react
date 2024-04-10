dc149974802ed0d56ed57aad45bee551534e8f72
ashwin-kumar06 <113244069+ashwin-kumar06@users.noreply.github.com>
2024-04-10 17:13:44 +0530

    Update Skill.js

--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -19,9 +19,6 @@ export default function Skill() {
               <li class="nav-item ">
                 <a class="nav-link " href="/home">Home</a>
               </li>
-              <li class="nav-item ">
-                <a class="nav-link" href="/contact">Contact</a>
-              </li>
             </ul>
           </div>
         </div>
