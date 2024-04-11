45e6553c514bf8eeac2d348865354e78787cff7c
ashwin <ashwinkumar0850@gmail.com>
2024-04-11 17:46:43 +0530

    emote

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -83,7 +83,14 @@ export default function Home() {
           </div>
         </div>
       </div>
-
+      <figure class="icon-cards mt-3">
+        <div class="icon-cards__content">
+          <div class="icon-cards__item d-flex align-items-center justify-content-center"><span class="h1">🙂</span></div>
+          <div class="icon-cards__item d-flex align-items-center justify-content-center"><span class="h1">😊</span></div>
+          <div class="icon-cards__item d-flex align-items-center justify-content-center"><span class="h1">😀</span></div>
+        </div>
+      </figure>
+      <script src="script.js"></script>
     </div>
   );
 }
