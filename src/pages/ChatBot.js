fcd4eec2535d6e4c6be43b23c69deed9eaab8ec1
ashwin <ashwinkumar0850@gmail.com>
2024-04-26 13:57:30 +0530

    bot removed

--- src/pages/ChatBot.js
+++ src/pages/ChatBot.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export default function ChatBot() {
     return (
         <div>
-            <div class="tenor-gif-embed" data-postid="21544397" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/o2-bubl-o2bubl-funny-cute-gif-21544397">O2 Bubl Sticker</a>from <a href="https://tenor.com/search/o2-stickers">O2 Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         </div>
     );
 };
